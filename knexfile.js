0779c92869ba244244e31709f4238b6a734bd6d8
meera37 <meerabenny370@gmail.com>
2025-08-22 12:14:28 +0530

    updated

--- knexfile.js
+++ knexfile.js
@@ -32,6 +32,41 @@ export default {
   }
 };
 
+
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const baseConfig = {
+//   client: process.env.DB_CLIENT || 'pg',
+//   migrations: {
+//     directory: './migrations'
+//   }
+// };
+
+// export default {
+//   development: {
+//     ...baseConfig,
+//     connection: {
+//       host: process.env.DB_HOST || '127.0.0.1',
+//       port: process.env.DB_PORT || 5432,
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME
+//     }
+//   },
+//   production: {
+//     ...baseConfig,
+//     connection: {
+//       host: process.env.DB_HOST,
+//       port: process.env.DB_PORT,
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME,
+//       ssl: { rejectUnauthorized: false }
+//     }
+//   }
+// };
+
 // import dotenv from 'dotenv';
 // dotenv.config();
 
