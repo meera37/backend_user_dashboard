29117d77cf02b98c58397817c86ed2bd04a40235
meera37 <meerabenny370@gmail.com>
2025-08-22 12:56:10 +0530

    updated migration file

--- migrations/20250819151319_create_users_table.js
+++ migrations/20250819151319_create_users_table.js
@@ -1,4 +1,4 @@
-exports.up = function(knex) {
+export function up(knex) {
   return knex.schema.createTable('users', table => {
     table.increments('id').primary();
     table.string('name').notNullable();
@@ -7,9 +7,9 @@ exports.up = function(knex) {
     table.boolean('deleted').defaultTo(false);
     table.timestamps(true, true);
   });
-};
+}
 
-exports.down = function(knex) {
+export function down(knex) {
   return knex.schema.dropTable('users');
-};
+}
 
