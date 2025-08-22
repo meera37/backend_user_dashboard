bbc1da0c95c24bd24da7d1399085b2b5fee792b0
meera37 <meerabenny370@gmail.com>
2025-08-22 12:49:19 +0530

    update db config for render postgres

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const baseConfig = {
-  client: process.env.DB_CLIENT || 'pg',
+  client: process.env.DB_CLIENT,
   migrations: {
     directory: './migrations'
   }
@@ -12,8 +12,8 @@ export default {
   development: {
     ...baseConfig,
     connection: {
-      host: process.env.DB_HOST || '127.0.0.1',
-      port: process.env.DB_PORT || 5432,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
@@ -27,95 +27,7 @@ export default {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      ssl: { rejectUnauthorized: false }
+      ssl: { rejectUnauthorized: false } // Important for Render
     }
   }
 };
-
-
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// const baseConfig = {
-//   client: process.env.DB_CLIENT || 'pg',
-//   migrations: {
-//     directory: './migrations'
-//   }
-// };
-
-// export default {
-//   development: {
-//     ...baseConfig,
-//     connection: {
-//       host: process.env.DB_HOST || '127.0.0.1',
-//       port: process.env.DB_PORT || 5432,
-//       user: process.env.DB_USER,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_NAME
-//     }
-//   },
-//   production: {
-//     ...baseConfig,
-//     connection: {
-//       host: process.env.DB_HOST,
-//       port: process.env.DB_PORT,
-//       user: process.env.DB_USER,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_NAME,
-//       ssl: { rejectUnauthorized: false }
-//     }
-//   }
-// };
-
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// export default {
-//   development: {
-//     client: process.env.DB_CLIENT,
-//     connection: {
-//       host: process.env.DB_HOST,
-//       port: process.env.DB_PORT,
-//       user: process.env.DB_USER,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_NAME
-//     },
-//     migrations: {
-//       directory: './migrations'
-//     }
-//   },
-//   production: {
-//     client: process.env.DB_CLIENT,
-//     connection: {
-//       host: process.env.DB_HOST,
-//       port: process.env.DB_PORT,
-//       user: process.env.DB_USER,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_NAME,
-//       ssl: { rejectUnauthorized: false }
-//     },
-//     migrations: {
-//       directory: './migrations'
-//     }
-//   }
-// };
-
-
-
-// require('dotenv').config();
-
-// module.exports = {
-//   development: {
-//     client: process.env.DB_CLIENT,
-//     connection: {
-//       host: process.env.DB_HOST,
-//       port: process.env.DB_PORT,
-//       user: process.env.DB_USER,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_NAME
-//     },
-//     migrations: {
-//       directory: './migrations'
-//     }
-//   }
-// };
